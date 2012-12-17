cc9cd92416b5666ae66a2ee00eba36bb1b93eead
David Rajchenbach-Teller <dteller@mozilla.com>
2012-12-17 21:44:19 +0100

    fix

--- js/bug.js
+++ js/bug.js
@@ -2,7 +2,15 @@
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "img/large_image_from_wikipedia.jpg", true);
   xhr.responseType = "moz-blob";
+  xhr.onprogress = function(evt) {
+    if (evt.lengthComputable) {
+      console.log("progress", evt.loaded / evt.total);
+    } else {
+      console.log("progress");
+    }
+  };
   xhr.onload = function() {
+    console.log("xhr complete");
     var reader = new FileReader();
     reader.onload = function() {
       console.log("About to set src");
