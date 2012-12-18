1b508035822e1cccc7525fa28fb52d0547c8db9a
David Rajchenbach-Teller <dteller@mozilla.com>
2012-12-18 07:13:47 +0100

    [Nicety] Nicer progress display

--- js/bug.js
+++ js/bug.js
@@ -1,4 +1,5 @@
 (function() {
+  var eltLoading = document.getElementById("loading");
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "img/large_image_from_wikipedia.jpg", true);
   xhr.responseType = "moz-blob";
@@ -8,19 +9,23 @@
       var newProgress = Math.round((evt.loaded * 100) / evt.total);
       if (newProgress > progress) {
         progress = newProgress;
-        console.log("progress", newProgress);
+        eltLoading.textContent = "Downloading: " + newProgress + "%";
+        console.log("xhr progress", newProgress);
       }
     } else {
       console.log("progress");
     }
   };
   xhr.onload = function() {
+    eltLoading.textContent = "Converting to data URL";
     console.log("xhr complete");
     var reader = new FileReader();
     reader.onload = function() {
+      eltLoading.textContent = "Showing on screen";
       console.log("About to set src");
       document.getElementById("img").src = reader.result;
       console.log("I have set src");
+      eltLoading.textContent = "Load complete";
     };
     reader.readAsDataURL(xhr.response);
   };
