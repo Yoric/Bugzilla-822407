9cb28a56810737ea671ef608d080fbaba3bd2748
David Rajchenbach-Teller <dteller@mozilla.com>
2012-12-18 05:28:57 +0100

    [Nicety] Nicer progress display

--- js/bug.js
+++ js/bug.js
@@ -2,9 +2,14 @@
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "img/large_image_from_wikipedia.jpg", true);
   xhr.responseType = "moz-blob";
+  var progress = 0;
   xhr.onprogress = function(evt) {
     if (evt.lengthComputable) {
-      console.log("progress", evt.loaded / evt.total);
+      var newProgress = Math.round((evt.loaded * 100) / evt.total);
+      if (newProgress > progress) {
+        progress = newProgress;
+        console.log("progress", newProgress);
+      }
     } else {
       console.log("progress");
     }
